f9ddbfcd2ed15e07406f1ef594e85fda020bbba5
KeerthiMalini05 <58335834+KeerthiMalini05@users.noreply.github.com>
2022-06-23 14:04:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider (GCP, AWS, Azure)
 provider "google" {
-credentials = "${file("credentials.json")}"
+credentials = "${file("sonorous-folio-264504-2efd1155d4b6.json")}"
 project = "ac-shared-playground"
 region = "us-central1"
 }
